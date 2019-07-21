6c3b941207a053d50082f581aa9a86a752bb8171
Julian Norton <julian@juliannorton.com>
2019-07-21 10:50:18 -0400

    this.matter undefined error

--- matter-ball.js
+++ matter-ball.js
@@ -3,7 +3,7 @@ var config = {
     width: 400,
     height: 400,
     backgroundColor: "#666666",
-    parent: 'game',
+    parent: 'game-container',
     physics: {
         default: 'matter',
         matter: {
@@ -27,20 +27,26 @@ function preload() {
 
 function create() {
 
-    this.add.image(100, 100, 'placeholder');
-    this.add.image(50, 50, '');
+    var image1 = this.add.image(100, 100, 'placeholder');
+    var image2 = this.add.image(50, 50, '');
+
+    container = this.add.container(100, 100, [image1, image2]);
 
     var rectangle = this.add.rectangle(250, 380, 200, 5, { isStatic: true })
     var ball = this.add.circle(300, 100, 30, 1)
-    MatterPhysics.Bodies.rectangle(200, 100, 60, 60, { frictionAir: 0.001 }),
-    MatterPhysics.Bodies.rectangle(400, 100, 60, 60, { frictionAir: 0.05 }),
-    MatterPhysics.Bodies.rectangle(600, 100, 60, 60, { frictionAir: 0.1 }),
 
-    // m/ walls
-    MatterPhysics.Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-    MatterPhysics.Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
-    MatterPhysics.Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
-    MatterPhysics.Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+    container.setSize(128, 64);
+    var physicsContainer = this.matter.add.gameObject(container);
+
+    // MatterPhysics.Bodies.rectangle(200, 100, 60, 60, { frictionAir: 0.001 }),
+    // MatterPhysics.Bodies.rectangle(400, 100, 60, 60, { frictionAir: 0.05 }),
+    // MatterPhysics.Bodies.rectangle(600, 100, 60, 60, { frictionAir: 0.1 }),
+
+    // // m/ walls
+    // MatterPhysics.Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+    // MatterPhysics.Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+    // MatterPhysics.Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+    // MatterPhysics.Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
 
 }
 
