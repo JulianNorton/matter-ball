4eff1bd823442a10a95392c92548d260fd843231
Julian Norton <julian@juliannorton.com>
2019-07-21 10:13:16 -0400

    Trying to figure out matter.

---
+++ matter-ball.js
@@ -0,0 +1,52 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 400,
+    height: 400,
+    backgroundColor: "#666666",
+    parent: 'game',
+    physics: {
+        default: 'matter',
+        matter: {
+            debug: true,
+            gravity: { y: .5 },
+            wireframes: true,
+            showAngleIndicator: true,
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('placeholder', 'assets/placeholder-image.png');
+}
+
+function create() {
+
+    this.add.image(100, 100, 'placeholder');
+    this.add.image(50, 50, '');
+
+    var rectangle = this.add.rectangle(250, 380, 200, 5, { isStatic: true })
+    var ball = this.add.circle(300, 100, 30, 1)
+    MatterPhysics.Bodies.rectangle(200, 100, 60, 60, { frictionAir: 0.001 }),
+    MatterPhysics.Bodies.rectangle(400, 100, 60, 60, { frictionAir: 0.05 }),
+    MatterPhysics.Bodies.rectangle(600, 100, 60, 60, { frictionAir: 0.1 }),
+
+    // m/ walls
+    MatterPhysics.Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+    MatterPhysics.Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+    MatterPhysics.Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+    MatterPhysics.Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+
+}
+
+function update() {
+    // cursors = this.input.keyboard.createCursorKeys();
+    // if (cursors.spacebar.isDown) {
+    // player.setVelocityX(-160);
+    // }
+}
