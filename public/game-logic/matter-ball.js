b824ba09a97143a27c45de7fe3eb9317bee671e9
Julian Norton <julian@juliannorton.com>
2019-07-27 14:20:27 -0400

    succesfully destroying player object on disconnet

--- public/game-logic/matter-ball.js
+++ public/game-logic/matter-ball.js
@@ -31,10 +31,24 @@ function preload() {
 function create() {
     var self = this;
     this.socket = io();
+    this.otherPlayers = this.add.group();
     this.socket.on('currentPlayers', function (players) {
         Object.keys(players).forEach(function (id) {
             if (players[id].playerId === self.socket.id) {
                 addPlayer(self, players[id]);
+            } else {
+                addOtherPlayers(self, players[id]);
+            }
+        });
+    });
+    this.socket.on('newPlayer', function (playerInfo) {
+        addOtherPlayers(self, playerInfo);
+    });
+    this.socket.on('disconnect', function (playerId) {
+        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+            if (playerId === otherPlayer.playerId) {
+                otherPlayer.destroy();
+                console.log('destroyed player', playerId)
             }
         });
     });
@@ -55,8 +69,8 @@ function create() {
     player = this.matter.add.image(200, 200, 'placeholder').setScale(1,1);
     // player.setFixedRotation();
     player.setAngle(270);
-    player.setFrictionAir(.1);
-    player.setMass(50);
+    player.setFrictionAir(.2);
+    player.setMass(20);
 
     // Set-up keyboard input
     cursors = this.input.keyboard.createCursorKeys();
@@ -65,20 +79,42 @@ function create() {
 
 function update() {
     if (cursors.left.isDown) {
-        player.thrustLeft(1);
+        player.thrustLeft(.5);
     }
     else if (cursors.right.isDown) {
-        player.thrustRight(1);
+        player.thrustRight(.5);
     }
 
     if (cursors.up.isDown) {
-        player.thrust(1);
+        player.thrust(.5);
     }
     else if (cursors.down.isDown) {
-        player.thrustBack(1);
+        player.thrustBack(.5);
     }
     // ms delay on adding circle
     if (this.input.keyboard.checkDown(cursors.space, 500)) {
         this.matter.add.circle(100, 50, 32, 0);
     }
+}
+
+function addPlayer(self, playerInfo) {
+    // player = this.matter.add.image(200, 200, 'placeholder').setScale(1, 1);
+
+    self.ship = self.matter.add.image(playerInfo.x, playerInfo.y, 'placeholder').setOrigin(0.5, 0.5).setDisplaySize(60, 40);
+    if (playerInfo.team === 'team_one') {
+        self.ship.setTint(0x00ffff);
+    } else {
+        self.ship.setTint(0xff0000);
+    }
+}
+
+function addOtherPlayers(self, playerInfo) {
+    const otherPlayer = self.matter.add.image(playerInfo.x, playerInfo.y, 'placeholder').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+    if (playerInfo.team === 'team_one') {
+        otherPlayer.setTint(0xff00ff);
+    } else {
+        otherPlayer.setTint(0xffff00);
+    }
+    otherPlayer.playerId = playerInfo.playerId;
+    self.otherPlayers.add(otherPlayer);
 }
